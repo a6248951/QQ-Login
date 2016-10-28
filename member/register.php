d78cb941fbdd9410143fd1c9c0c8c1d855f2ea15
iwen <2643052714@qq.com>
2016-10-28 18:32:44 +0800

    Update register.php

--- member/register.php
+++ member/register.php
@@ -1,17 +1,13 @@
 <?php 
 require 'config.inc.php';
 require '../common.inc.php';
-
 require AJ_ROOT.'/module/'.$module.'/register.inc.php';
-
 //回调页面做QQ授权处理
-
 //1,QQ授权CODE
 $appid = '101346137'; 
 $appkey = 'eb5252e19ed87a0b4ca3ae6a566e28b7';
 $redirect_uri = 'http://geijiachina.com/member/register.php';
 $redirect_uri=urlencode($redirect_uri);
-
 //2,获取token ，从而获取access_token,用于获取续期token
 $url = "https://graph.qq.com/oauth2.0/token?grant_type=authorization_code&client_id=101346137&client_secret=eb5252e19ed87a0b4ca3ae6a566e28b7&code=".$_GET['code']."&redirect_uri=".$redirect_uri."";
 $str=file_get_contents($url);
@@ -32,26 +28,14 @@ $lpos = strpos($strww, "(");
 $rpos = strrpos($strww, ")");
 $response = substr($strww, $lpos + 1, $rpos - $lpos -1);
 $user = json_decode($response,true);
-
 $urlff = "https://graph.qq.com/user/get_user_info?access_token=".$e."&oauth_consumer_key=101346137&openid=".$user['openid']."";
 $strff=file_get_contents($urlff);
-var_dump($strff);
-echo "<br/>";
-echo "<br/>";
-
 $data = json_decode($strff,true);
-
 var_dump($user['openid']);
 echo "<br/>";
 var_dump($data['nickname']);
 echo "<br/>";
 
-
-//var_dump($data['gender'])；
-//echo  "<br/>";
-//var_dump($data['province'])；
-//echo "<br/>";
-//var_dump($data['city'])；
 
 
 
